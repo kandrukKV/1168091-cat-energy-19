9eeb048df9558f5586794e52f3e6042dca1657f5
Konstantin <kandruk@ya.ru>
2020-04-10 02:12:22 +0300

    DZ module4-task1

---
+++ source/js/script.js
@@ -0,0 +1,22 @@
+var navMain = document.querySelector('.main-nav');
+var pageHeader = document.querySelector('.page-header');
+var hamburger = document.querySelector('.hamburger');
+
+pageHeader.classList.remove('page-header--nojs');
+
+hamburger.addEventListener('click', function () {
+
+  if (navMain.classList.contains('main-nav--close')) {
+    navMain.classList.remove('main-nav--close');
+    navMain.classList.add('main-nav--open');
+    hamburger.classList.remove('hamburger--close');
+    hamburger.classList.add('hamburger--open');
+
+  } else {
+    navMain.classList.add('main-nav--close');
+    navMain.classList.remove('main-nav--open');
+    hamburger.classList.add('hamburger--close');
+    hamburger.classList.remove('hamburger--open');
+  }
+
+});
